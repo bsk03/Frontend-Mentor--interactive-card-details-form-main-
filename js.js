31ffb1c89d53f24bfbcba241959943571ec6165e
Błażej Kowalczyk <blazej2k3@gmail.com>
2022-08-27 13:25:43 +0200

    download vars

---
+++ js.js
@@ -0,0 +1,19 @@
+// card
+const cardNum = document.querySelector('.cardNum');
+const cardName = document.querySelector('.name');
+const cardExpireDate = document.querySelector('.expireDate');
+const cardSecCode = document.querySelector('.secCode');
+// form values
+const formName = document.querySelector('.formName');
+const formNums = document.querySelector('.cNums');
+const formExpMonth = document.querySelector('.cMonth');
+const formExpYear = document.querySelector('.cYear');
+const formSecCode = document.querySelector('.cCvc');
+// errors
+const nameError = document.querySelector('.nameError');
+const cardNumError = document.querySelector('.cardNumError');
+const expError = document.querySelector('.expError');
+const secError = document.querySelector('.cvcError');
+// button
+const confirm = document.querySelector('.confirm');
+const continuee = document.querySelector('.continue')
